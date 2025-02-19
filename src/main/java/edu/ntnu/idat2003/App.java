10f53e5c63f65ed68bda0b3c650360137f3e7c8f
Segward <gustask@stud.ntnu.no>
2025-02-19 16:49:02 +0100

    try catch in App.java

--- src/main/java/edu/ntnu/idat2003/App.java
+++ src/main/java/edu/ntnu/idat2003/App.java
@@ -12,30 +12,38 @@ import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
-    // Create a list of commands
     ArrayList<TextCommand> commands = new ArrayList<>();
-    ReplaceTextCommand command1 = new ReplaceTextCommand("Hello", "hi");
-    ReplaceFirstTextCommand command2 = new ReplaceFirstTextCommand("world", "universe");
-    WrapTextCommand command3 = new WrapTextCommand("#", "#");
-    WrapSelectionTextCommand command4 = new WrapSelectionTextCommand("(", ")", "love");
-    WrapLinesTextCommand command5 = new WrapLinesTextCommand("...", "...");
-    CapitalizeTextCommand command6 = new CapitalizeTextCommand();
-    CapitalizeSelectionTextCommand command7 = new CapitalizeSelectionTextCommand("love");
-    CapitalizeWordsTextCommand command8 = new CapitalizeWordsTextCommand();
+    try {
+      // Create a list of commands
+      ReplaceTextCommand command1 = new ReplaceTextCommand("Hello", "hi");
+      ReplaceFirstTextCommand command2 = new ReplaceFirstTextCommand("world", "universe");
+      WrapTextCommand command3 = new WrapTextCommand("#", "#");
+      WrapSelectionTextCommand command4 = new WrapSelectionTextCommand("(", ")", "love");
+      WrapLinesTextCommand command5 = new WrapLinesTextCommand("...", "...");
+      CapitalizeTextCommand command6 = new CapitalizeTextCommand();
+      CapitalizeSelectionTextCommand command7 = new CapitalizeSelectionTextCommand("love");
+      CapitalizeWordsTextCommand command8 = new CapitalizeWordsTextCommand();
 
-    // Add the commands to the list
-    commands.add(command1);
-    commands.add(command2);
-    commands.add(command3);
-    commands.add(command4);
-    commands.add(command5);
-    commands.add(command6);
-    commands.add(command7);
-    commands.add(command8);
+      // Add the commands to the list
+      commands.add(command1);
+      commands.add(command2);
+      commands.add(command3);
+      commands.add(command4);
+      commands.add(command5);
+      commands.add(command6);
+      commands.add(command7);
+      commands.add(command8);
+    } catch (Exception e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
 
-    // Create a script with the list of commands
-    Script script = new Script(commands);
-    script.execute(
-        "hello World, I love programming!\n I also love omniscient reader's viewpoint.\n bye bye!");
+    if (!commands.isEmpty()) {
+      Script script = new Script(commands);
+      script.execute(
+          "hello World, I love programming!\n"
+              + " I also love omniscient reader's viewpoint.\n"
+              + " bye bye!");
+    }
   }
 }
